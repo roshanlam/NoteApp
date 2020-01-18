dc27a3bdc3bf7db584bdaa4c857141b5b9ea065a
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-18 11:29:40 -0500

    Create Study.java

---
+++ Study.java
@@ -0,0 +1,12 @@
+package study;
+import gui.Reader;
+import structures.Book;
+import structures.Section;
+
+public class Study {
+    public static void main(String[] args) {
+        Reader notetaker = new Reader();
+        notetaker.setVisible(true);
+    }
+    
+}
